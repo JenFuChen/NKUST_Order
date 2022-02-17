438faaed279b638ae17a6ff1d2a2379553fd1287
Chen Jenfu <dht98010@gmail.com>
2022-02-17 15:05:56 +0800

    Initial Commit

---
+++ NKUST_Order/NKUST_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NKUST_OrderApp.swift
+//  NKUST_Order
+//
+//  Created by 陳人輔 on 2022/2/17.
+//
+
+import SwiftUI
+
+@main
+struct NKUST_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
